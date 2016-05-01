e5df6f831ed202e90e76ed4351a1357a72dce122
Derek Phillips <d2philli@gmail.com>
2016-04-30 17:34:05 -0700

    testing heroku auto update

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'hello world!!!! not'
-
+	return 'THIS IS A POST TO GITHUB, SHOULD UPDATE ON HEROKU'
 if __name__ == '__main__':
 	app.run()
